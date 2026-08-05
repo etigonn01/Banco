a3ee994968d1181e0723380fb1a6185b06087c91
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-05 17:52:39 -0600

    Inicio de proyecto y creación de el modelo User

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simulador de Banco
+
+## Tecnologias
+- Java SE 21
+- Spring Boot
+- PostgreSQL 15
+
+## Funciones
+Esta aplicación expone puntos de acceso para realizar operaciones básicas y contidianas de un banco.
+Por ejemplo depositar efectivo en cuenta, retirar efectivo, transferir a otra cuenta.
---
+++ src/main/java/com/siqueiros/bank/model/User.java
@@ -0,0 +1,77 @@
+package com.siqueiros.bank.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fullname", nullable = false, length = 120)
+    private String fullname;
+
+    @Column(name = "email", unique = true, length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Column(name = "phone_number", nullable = false, unique = true, length = 10)
+    private String phoneNumber;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}

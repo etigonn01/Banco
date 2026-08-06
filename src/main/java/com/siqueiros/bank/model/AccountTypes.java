dbd12c342239f782242827368b5d214e39981624
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-05 18:11:49 -0600

    feat: desarrolo de la Entidad AccountTypes

---
+++ src/main/java/com/siqueiros/bank/model/AccountTypes.java
@@ -0,0 +1,32 @@
+package com.siqueiros.bank.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account_types")
+public class AccountTypes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 30)
+    private String type;
+
+    public AccountTypes() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

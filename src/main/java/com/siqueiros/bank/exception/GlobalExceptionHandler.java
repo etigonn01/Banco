e886c2633f6e93945c1bb8dad680b536e955b6a4
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-06 18:10:35 -0600

    feat: creación de una excepcion personalizada para recurso no encontrados en la entidad User y desarrollo de un GobalHandlerException

--- src/main/java/com/siqueiros/bank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/siqueiros/bank/exception/GlobalExceptionHandler.java
@@ -1,4 +1,50 @@
 package com.siqueiros.bank.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.siqueiros.bank.dto.ErrorResponseDTO;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleUserNotFound(UserNotFoundException ex, WebRequest request) {
+        String cleanPath = request.getDescription(false).replace("uri=", "");
+        ErrorResponseDTO errorBody = new ErrorResponseDTO(
+                cleanPath,
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorBody, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDataIntegrityViolation(
+            DataIntegrityViolationException ex, WebRequest request
+    ) {
+        String cleanPath = request.getDescription(false).replace("uri=", "");
+
+        String message = "El correo electrónico proporcionado ya está registrado en el sistema.";
+
+        if(ex.getMostSpecificCause().getMessage().contains("phone")) {
+            message = "El número de teléfono proporcionado ya está registrado en el sistema.";
+        }
+
+        ErrorResponseDTO errorBody = new ErrorResponseDTO(
+                cleanPath,
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                message,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorBody, HttpStatus.CONFLICT);
+    }
 }

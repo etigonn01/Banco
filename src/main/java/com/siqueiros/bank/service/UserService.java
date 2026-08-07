5a104b5d9c72e43013ed46d5d30c6b1e77d93976
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-06 18:16:12 -0600

    feat: desarrollo e implentación de interfaz para el servicio de la entidad User

--- src/main/java/com/siqueiros/bank/service/UserService.java
+++ src/main/java/com/siqueiros/bank/service/UserService.java
@@ -1,13 +1,16 @@
 package com.siqueiros.bank.service;
 
+import com.siqueiros.bank.dto.UserRequestDTO;
+import com.siqueiros.bank.dto.UserResponseDTO;
 import com.siqueiros.bank.model.User;
 import java.util.Optional;
 import java.util.List;
 
 public interface UserService {
-    List<User> getAll();
-    Optional<User> getById(Long id);
-    User save(User user);
-    Optional<User> delete(Long id);
-    Optional<User> getByEmail(String email);
+    List<UserResponseDTO> getAllUsers();
+    UserResponseDTO getUserById(Long id);
+    UserResponseDTO createUser(UserRequestDTO request);
+    UserResponseDTO deleteUser(Long id);
+    UserResponseDTO getUserByEmail(String email);
+    UserResponseDTO updateUser(Long id, UserRequestDTO request);
 }

e59d0bb0e34e74b557a0c2ca79b851b5756421e3
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-06 18:18:38 -0600

    feat: desarrollo de DTOs de petición y respuesta para el UserController

--- src/main/java/com/siqueiros/bank/dto/UserRequestDTO.java
+++ src/main/java/com/siqueiros/bank/dto/UserRequestDTO.java
@@ -1,4 +1,4 @@
 package com.siqueiros.bank.dto;
 
-public record UserRequest(String fullname, String email, String passwordHash, String phoneNumber) {
+public record UserRequestDTO(String fullname, String email, String passwordHash, String phoneNumber) {
 }

59a4b60c44fb2f268394e43946b96504da832020
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-06 18:17:19 -0600

    feat: desarrollo de un DTO de respuesta para el controlador ApiInfoController

--- src/main/java/com/siqueiros/bank/dto/ApiInfoResponseDTO.java
+++ src/main/java/com/siqueiros/bank/dto/ApiInfoResponseDTO.java
@@ -2,5 +2,5 @@ package com.siqueiros.bank.dto;
 
 import java.util.List;
 
-public record ApiInfoResponse(String greeting, String version, String author, List<String> technologies) {
+public record ApiInfoResponseDTO(String greeting, String version, String author, List<String> technologies) {
 }

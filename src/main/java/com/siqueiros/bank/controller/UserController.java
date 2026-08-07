15b80e13fcc188b39431ba4f8b71ba5aa1ced392
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-06 18:14:46 -0600

    feat: desarrollo de puntos de acceso para UserController

--- src/main/java/com/siqueiros/bank/controller/UserController.java
+++ src/main/java/com/siqueiros/bank/controller/UserController.java
@@ -1,4 +1,57 @@
 package com.siqueiros.bank.controller;
 
+import com.siqueiros.bank.dto.UserRequestDTO;
+import com.siqueiros.bank.dto.UserResponseDTO;
+import com.siqueiros.bank.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<UserResponseDTO> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/search/{id}")
+    public UserResponseDTO getUserById(@PathVariable Long id) {
+        UserResponseDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user).getBody();
+    }
+
+    @GetMapping("/search")
+    public UserResponseDTO getUserByEmail(@RequestParam String email) {
+        UserResponseDTO user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user).getBody();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO request) {
+        UserResponseDTO response = userService.createUser(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserRequestDTO request) {
+        UserResponseDTO updatedUser = userService.updateUser(id, request);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> deleteUser(@PathVariable Long id) {
+        UserResponseDTO user = userService.deleteUser(id);
+        return ResponseEntity.ok(user);
+    }
+
 }

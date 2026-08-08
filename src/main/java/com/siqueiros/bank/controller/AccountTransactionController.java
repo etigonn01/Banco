3d71fe3920d6d948741a42076e1b892cefe75dde
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-07 23:19:08 -0600

    feat: creación del controlador AccountTransaction

--- src/main/java/com/siqueiros/bank/controller/AccountTransactionController.java
+++ src/main/java/com/siqueiros/bank/controller/AccountTransactionController.java
@@ -1,4 +1,29 @@
 package com.siqueiros.bank.controller;
 
+import com.siqueiros.bank.dto.TransactionRequestDTO;
+import com.siqueiros.bank.dto.TransactionResponseDTO;
+import com.siqueiros.bank.service.AccountTransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
 public class AccountTransactionController {
+    private final AccountTransactionService transactionService;
+
+    public AccountTransactionController(AccountTransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransactionResponseDTO create(@RequestBody TransactionRequestDTO request) {
+        return transactionService.createTransaction(request);
+    }
+
+    @GetMapping("/{accountId}")
+    public List<TransactionResponseDTO> getAllTransactionsByAccountId(@PathVariable Long accountId) {
+        return transactionService.getTransactionsByAccountId(accountId);
+    }
 }

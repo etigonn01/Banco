374bac05aa477e8aec614b8d04b814f041f85074
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-07 23:18:02 -0600

    feat: creación e implementación del servicio de AccountTransactions

--- src/main/java/com/siqueiros/bank/service/AccountTransactionService.java
+++ src/main/java/com/siqueiros/bank/service/AccountTransactionService.java
@@ -1,4 +1,11 @@
 package com.siqueiros.bank.service;
 
+import com.siqueiros.bank.dto.TransactionRequestDTO;
+import com.siqueiros.bank.dto.TransactionResponseDTO;
+
+import java.util.List;
+
 public interface AccountTransactionService {
+    TransactionResponseDTO createTransaction(TransactionRequestDTO dto);
+    List<TransactionResponseDTO> getTransactionsByAccountId(Long accountId);
 }

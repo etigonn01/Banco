100da19fb2908b1c1db64b7c22d72e8f0acbecd5
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-07 23:19:46 -0600

    refactor: cambio del nombre de la interfaz

--- src/main/java/com/siqueiros/bank/repositories/TypeOperationRepository.java
+++ src/main/java/com/siqueiros/bank/repositories/TypeOperationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OperationTypeRepository extends JpaRepository<TypeOperation, Long>{
+public interface TypeOperationRepository extends JpaRepository<TypeOperation, Long>{
 }

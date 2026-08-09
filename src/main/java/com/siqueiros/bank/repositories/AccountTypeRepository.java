7debdf8181845197859b762c3401366785aefea2
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-08 21:55:29 -0600

    refactor: cambiando nombre del modelo AccounType para TypeAccount

--- src/main/java/com/siqueiros/bank/repositories/AccountTypeRepository.java
+++ src/main/java/com/siqueiros/bank/repositories/AccountTypeRepository.java
@@ -1,9 +1,9 @@
 package com.siqueiros.bank.repositories;
 
-import com.siqueiros.bank.model.AccountType;
+import com.siqueiros.bank.model.TypeAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountTypeRepository extends JpaRepository<AccountType, Long>{
+public interface AccountTypeRepository extends JpaRepository<TypeAccount, Long>{
 }

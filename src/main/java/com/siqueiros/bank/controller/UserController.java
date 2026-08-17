cfdfe9c0accedc29c94950013ae88583ae53dda9
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-16 21:20:43 -0600

    refactor: mejorando la legibilidad del código del controlador

--- src/main/java/com/siqueiros/bank/controller/UserController.java
+++ src/main/java/com/siqueiros/bank/controller/UserController.java
@@ -5,16 +5,7 @@ import com.siqueiros.bank.dto.UserResponseDTO;
 import com.siqueiros.bank.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -27,40 +18,41 @@ public class UserController {
     }
 
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public List<UserResponseDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/search/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public UserResponseDTO getUserById(@PathVariable Long id) {
-        UserResponseDTO user = userService.getUserById(id);
-        return ResponseEntity.ok(user).getBody();
+        return userService.getUserById(id);
     }
 
     @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
     public UserResponseDTO getUserByEmail(@RequestParam String email) {
-        UserResponseDTO user = userService.getUserByEmail(email);
-        return ResponseEntity.ok(user).getBody();
+        return userService.getUserByEmail(email);
     }
 
     @PostMapping
-    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO request) {
-        UserResponseDTO response = userService.createUser(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO createUser(@Valid @RequestBody UserRequestDTO request) {
+        return userService.createUser(request);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> updateUser(
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponseDTO updateUser(
             @PathVariable Long id,
             @Valid @RequestBody UserRequestDTO request) {
-        UserResponseDTO updatedUser = userService.updateUser(id, request);
-        return ResponseEntity.ok(updatedUser);
+        return userService.updateUser(id, request);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> deleteUser(@PathVariable Long id) {
-        UserResponseDTO user = userService.deleteUser(id);
-        return ResponseEntity.ok(user);
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponseDTO deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
     }
 
 }

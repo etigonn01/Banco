3f6b65ec5be2561e848884b7ad4a53df8f7a9104
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-20 18:52:41 -0600

    feat: implementando método de busqueda por Id con bloqueo pesimista para congelar la fila en la base de datos hasta que termine la transacción

--- src/main/java/com/siqueiros/bank/repositories/AccountRepository.java
+++ src/main/java/com/siqueiros/bank/repositories/AccountRepository.java
@@ -1,10 +1,19 @@
 package com.siqueiros.bank.repositories;
 
 import com.siqueiros.bank.model.Account;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
     boolean existsByUserIdAndTypeAccountId(Long userId, Long typeAccountId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.id = :id")
+    Optional<Account> findByIdWithLock(@Param("id") Long id);
 }

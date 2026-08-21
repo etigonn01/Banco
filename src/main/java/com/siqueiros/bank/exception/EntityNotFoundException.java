246817df141e739569b49d942c3843625bca9d82
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-20 19:53:18 -0600

    feat: implementando metodo estatico para levantar la excepción de forma más limpia

--- src/main/java/com/siqueiros/bank/exception/EntityNotFoundException.java
+++ src/main/java/com/siqueiros/bank/exception/EntityNotFoundException.java
@@ -4,4 +4,10 @@ public class EntityNotFoundException extends RuntimeException{
     public EntityNotFoundException(String message) {
         super(message);
     }
+
+    public static EntityNotFoundException of(String entityName, long id) {
+        return new EntityNotFoundException(
+                String.format("%s no encontrada con Id: %d", entityName, id)
+        );
+    }
 }

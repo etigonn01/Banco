abbf34482ffc66e021fbdec4bd11ae97fe16e0cb
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-20 19:49:41 -0600

    feat: implementado fabrica para seleccionar el tipo de operacion

--- src/main/java/com/siqueiros/bank/enums/OperationStrategyFactory.java
+++ src/main/java/com/siqueiros/bank/enums/OperationStrategyFactory.java
@@ -1,4 +1,18 @@
 package com.siqueiros.bank.enums;
 
+import java.util.List;
+
 public class OperationStrategyFactory {
+    private final List<OperationStrategy> strategies;
+
+    public OperationStrategyFactory(List<OperationStrategy> strategies) {
+        this.strategies = strategies;
+    }
+
+    public OperationStrategy getStrategy(String operationName) {
+        return strategies.stream()
+                .filter(s -> s.supports(operationName))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Operación no disponible"));
+    }
 }

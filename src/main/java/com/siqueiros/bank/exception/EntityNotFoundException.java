e24bc18ad442b60bb4cc4a50b7937fb543217da8
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-21 15:17:32 -0600

    feat: agregando metodo estatico para evitar usar la palabra new

--- src/main/java/com/siqueiros/bank/exception/EntityNotFoundException.java
+++ src/main/java/com/siqueiros/bank/exception/EntityNotFoundException.java
@@ -5,7 +5,7 @@ public class EntityNotFoundException extends RuntimeException{
         super(message);
     }
 
-    public static EntityNotFoundException of(String entityName, long id) {
+    public static EntityNotFoundException raise(String entityName, long id) {
         return new EntityNotFoundException(
                 String.format("%s no encontrada con Id: %d", entityName, id)
         );

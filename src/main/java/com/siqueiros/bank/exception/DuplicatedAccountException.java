c6a50c073cab181e3420d7faf78877789ee0559f
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-23 17:03:11 -0600

    refactor: renombrando el método de DuplactedAccountException de raise a of

--- src/main/java/com/siqueiros/bank/exception/DuplicatedAccountException.java
+++ src/main/java/com/siqueiros/bank/exception/DuplicatedAccountException.java
@@ -5,7 +5,7 @@ public class DuplicatedAccountException extends RuntimeException {
         super(message);
     }
 
-    public static DuplicatedAccountException raise(long id) {
+    public static DuplicatedAccountException of(long id) {
         return new DuplicatedAccountException(
                 String.format("La cuenta de origen con Id %d es la misma que la cuenta de destino. Esta opreación no está permitida", id)
         );

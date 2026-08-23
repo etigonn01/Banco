0eb9be3586c04aa05bc185eed56c19d29c106cdc
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-23 17:03:52 -0600

    feat: generando método of de InssufficientFundsException

--- src/main/java/com/siqueiros/bank/exception/InsufficientFundsException.java
+++ src/main/java/com/siqueiros/bank/exception/InsufficientFundsException.java
@@ -4,4 +4,10 @@ public class InsufficientFundsException extends RuntimeException {
     public InsufficientFundsException(String message) {
         super(message);
     }
+
+    public static InsufficientFundsException of(String balance) {
+        return new InsufficientFundsException(
+                String.format("El saldo es invalido. Saldo actual: $ %s", balance)
+        );
+    }
 }

5ec21aecee06ebefa389e78d51c2bed259170418
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-23 15:38:41 -0600

    feat: desarrollando método para hacer consulta a la base de datos y mapear relaciones de manera optimizada

--- src/main/java/com/siqueiros/bank/repositories/AccountRepository.java
+++ src/main/java/com/siqueiros/bank/repositories/AccountRepository.java
@@ -8,11 +8,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
+    @Query("SELECT a FROM Account a JOIN FETCH a.user JOIN FETCH a.typeAccount")
+    List<Account> findAllWithRelations();
+
     boolean existsByUserIdAndTypeAccountId(Long userId, Long typeAccountId);
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a FROM Account a WHERE a.id = :id")
     Optional<Account> findByIdWithRowLevelLocking(@Param("id") Long id);

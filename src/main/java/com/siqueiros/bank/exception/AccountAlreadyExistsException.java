4db10d520f81ea92f9f5cdfda93d18953f3a8f2f
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-24 19:56:22 -0600

    feat: agregar excepcion y metodo para ser levantada cuando se intenta crear una cuenta bancaria del mismo tipo de cuenta

--- src/main/java/com/siqueiros/bank/exception/AccountAlreadyExistsException.java
+++ src/main/java/com/siqueiros/bank/exception/AccountAlreadyExistsException.java
@@ -1,12 +1,12 @@
 package com.siqueiros.bank.exception;
 
-public class PayrollAccountAlreadyExistsException extends RuntimeException {
-    public PayrollAccountAlreadyExistsException(String message) {
+public class AccountAlreadyExistsException extends RuntimeException {
+    public AccountAlreadyExistsException(String message) {
         super(message);
     }
 
-    public static PayrollAccountAlreadyExistsException of(long id) {
-        return new PayrollAccountAlreadyExistsException(
+    public static AccountAlreadyExistsException of(long id) {
+        return new AccountAlreadyExistsException(
                 String.format("El usuario con Id: %d ya tiene registrada una cuenta de este tipo", id)
         );
     }

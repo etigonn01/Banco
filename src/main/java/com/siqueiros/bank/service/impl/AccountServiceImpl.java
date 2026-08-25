0b79b78feae6091aaa19573767b3b10ec7619186
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-24 19:49:50 -0600

    refactor: mover mapeo de entidades a DTO aplicando el patron DataMapper

--- src/main/java/com/siqueiros/bank/service/impl/AccountServiceImpl.java
+++ src/main/java/com/siqueiros/bank/service/impl/AccountServiceImpl.java
@@ -2,7 +2,7 @@ package com.siqueiros.bank.service.impl;
 
 import com.siqueiros.bank.dto.AccountRequestDTO;
 import com.siqueiros.bank.dto.AccountResponseDTO;
-import com.siqueiros.bank.exception.DuplicatedAccountException;
+import com.siqueiros.bank.exception.AccountAlreadyExistsException;
 import com.siqueiros.bank.exception.EntityNotFoundException;
 import com.siqueiros.bank.mappers.AccountMapper;
 import com.siqueiros.bank.model.Account;
@@ -34,28 +34,47 @@ public class AccountServiceImpl implements AccountService
         this.accountMapper = accountMapper;
     }
 
+    /**
+     * Devuelve todas las cuentas bancarias activas del la base de datos
+     * @return Una lista {@link List} de tipo {@link AccountResponseDTO} con los registros activos en la base de datos
+     */
     @Override
-    public List<AccountResponseDTO> findAllAccounts() {
-        return accountRepository.findAllWithRelations()
+    public List<AccountResponseDTO> findAllActiveAccounts() {
+
+        return accountRepository.findAllActiveWithRelations()
                 .stream()
                 .map(accountMapper::toResponse)
                 .toList();
     }
 
+    /**
+     * Devuelve la cuenta bancaria activa con el Id
+     * @param accountId ID de la cuenta que se busca en la base de datos
+     * @return Un objeto {@link AccountResponseDTO} con la información del registro activo encontrado
+     * @throws EntityNotFoundException Si el accountId no corresponde a ninguna cuenta en la base de datos
+     */
     @Override
-    public AccountResponseDTO findByAccountId(Long accountId) {
-        return accountRepository.findByIdWithRelations(accountId)
+    public AccountResponseDTO findActiveAccountById(Long accountId) {
+        return accountRepository.findActiveByIdWithRelations(accountId)
                 .map(accountMapper::toResponse)
                 .orElseThrow(() -> EntityNotFoundException.of("Cuenta",  accountId));
     }
 
+    /**
+     * Registra una cuenta bancaria en la base de datos
+     * @param accountRequestDTO Es el objeto que llega en la petición con la información para dar de alta una cuenta bancaria
+     * @return {@link AccountResponseDTO} - Es el objeto de respuesta con la información del usuario registrado
+     * @throws AccountAlreadyExistsException Si el usuario ya tiene registrada una cuenta del mismo tipo
+     * @throws EntityNotFoundException Si el userId no corresponde a ningun usuario en la base de datos
+     * @throws EntityNotFoundException Si el typeAccountId no corresponde a ningun tipo de cuenta de la base de datos
+     */
     @Override
     public AccountResponseDTO createAccount(AccountRequestDTO accountRequestDTO) {
-        boolean isDuplicatedAccount = accountRepository
-                .existsByUserIdAndTypeAccountId(accountRequestDTO.userId(), accountRequestDTO.typeAccountId());
+        boolean accountAlreadyExist = accountRepository
+                .existsActiveByUserIdAndTypeAccountId(accountRequestDTO.userId(), accountRequestDTO.typeAccountId());
 
-        if (isDuplicatedAccount) {
-            throw DuplicatedAccountException.of(accountRequestDTO.userId());
+        if (accountAlreadyExist) {
+            throw AccountAlreadyExistsException.of(accountRequestDTO.userId());
         }
 
         var user = userRepository.findById(accountRequestDTO.userId())
@@ -69,33 +88,18 @@ public class AccountServiceImpl implements AccountService
         return accountMapper.toResponse(accountSaved);
     }
 
-    @Override
-    public AccountResponseDTO updateAccount(Long accountId, Long typeAccountId) {
-        var typeAccount = typeAccountRepository.findById(typeAccountId)
-                .orElseThrow(() -> new EntityNotFoundException("Tipo de cuenta no  encontrada. Id: " + typeAccountId));
-
-        var account  = accountRepository.findById(accountId)
-                .orElseThrow(() -> new EntityNotFoundException("Cuenta no encontrada. Id: " + accountId));
-
-        account.setTypeAccount(typeAccount);
-        return mapToResponse(accountRepository.save(account));
-    }
-
+    /**
+     * Elimina de manera lógica una cuenta bancaria de la base de datos
+     * @param accountId ID de la cuenta que se desea eliminar de la base de datos
+     * @return Un objeto {@link AccountResponseDTO} con la información de la cuenta bancaria eliminada
+     * @throws EntityNotFoundException Si el accountId no corresponde a ninguna cuenta bancaria en la base de datos
+     */
     @Override
     public AccountResponseDTO deleteAccount(Long accountId) {
-        var account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new EntityNotFoundException("Cuenta no encontrada. Id: " + accountId));
-        accountRepository.delete(account);
-        return mapToResponse(account);
-    }
-
-    private AccountResponseDTO mapToResponse(Account account) {
-        return new AccountResponseDTO(
-                account.getId(),
-                account.getBalance(),
-                account.getTypeAccount().getName(),
-                account.getUser().getFullName(),
-                account.getCreatedAt()
-        );
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> EntityNotFoundException.of("Cuenta", accountId));
+        account.close();
+        accountRepository.save(account);
+        return accountMapper.toResponse(account);
     }
 }

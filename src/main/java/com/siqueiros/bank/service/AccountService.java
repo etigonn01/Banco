23d96b617ba23d04bbb369c5d9aa2b05ac5b4f6a
pedrosiqueiros <javierpedrolopez@gmail.com>
2026-08-24 19:30:53 -0600

    style: cambiar nombre de los métodos de la interfaz

--- src/main/java/com/siqueiros/bank/service/AccountService.java
+++ src/main/java/com/siqueiros/bank/service/AccountService.java
@@ -6,9 +6,8 @@ import com.siqueiros.bank.dto.AccountResponseDTO;
 import java.util.List;
 
 public interface AccountService {
-    List<AccountResponseDTO> findAllAccounts();
-    AccountResponseDTO findByAccountId(Long accountId);
+    List<AccountResponseDTO> findAllActiveAccounts();
+    AccountResponseDTO findActiveAccountById(Long accountId);
     AccountResponseDTO createAccount(AccountRequestDTO accountRequestDTO);
-    AccountResponseDTO updateAccount(Long accountId, Long typeAccountId);
     AccountResponseDTO deleteAccount(Long accountId);
 }
